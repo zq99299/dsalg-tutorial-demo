9fdce240f62bd61413337ae2999950f65d6976a4
zq99299 <99299684@qq.com>
2020-11-22 22:43:08 +0800

    查找

--- src/main/java/cn/mrcode/study/dsalgtutorialdemo/datastructure/tree/BinaryTreeTest.java
+++ src/main/java/cn/mrcode/study/dsalgtutorialdemo/datastructure/tree/BinaryTreeTest.java
@@ -75,6 +75,87 @@ public class BinaryTreeTest {
             // 3. 先输出当前节点
             System.out.println(this);
         }
+
+        /**
+         * 前序查找
+         */
+        public HeroNode preOrderSearch(int id) {
+            System.out.println("  进入前序遍历");  // 用来统计查找了几次
+            // 1. 如何相等，则返回
+            if (this.id == id) {
+                return this;
+            }
+            // 2. 如果左子节点不为空，则递归继续前序遍历
+            if (this.left != null) {
+                HeroNode result = this.left.preOrderSearch(id);
+                if (result != null) {
+                    return result;
+                }
+            }
+            // 3. 如果右子节点不为空，则递归继续前序遍历
+            if (this.right != null) {
+                HeroNode result = this.right.preOrderSearch(id);
+                if (result != null) {
+                    return result;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * 中序查找
+         */
+        public HeroNode infixOrderSearch(int id) {
+//            System.out.println("  进入中序遍历");  // 用来统计查找了几次，不能在这里打印，这里打印是进入了方法几次，看的是比较了几次
+            // 1. 如果左子节点不为空，则递归继续前序遍历
+            if (this.left != null) {
+                HeroNode result = this.left.infixOrderSearch(id);
+                if (result != null) {
+                    return result;
+                }
+            }
+            System.out.println("  进入中序遍历");
+            // 2. 如果相等，则返回
+            if (this.id == id) {
+                return this;
+            }
+
+            // 3. 如果右子节点不为空，则递归继续前序遍历
+            if (this.right != null) {
+                HeroNode result = this.right.infixOrderSearch(id);
+                if (result != null) {
+                    return result;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * 后序查找
+         */
+        public HeroNode postOrderSearch(int id) {
+//            System.out.println("  进入后序遍历");  // 用来统计查找了几次，不能在这里打印，这里打印是进入了方法几次，看的是比较了几次
+            // 1. 如果左子节点不为空，则递归继续前序遍历
+            if (this.left != null) {
+                HeroNode result = this.left.postOrderSearch(id);
+                if (result != null) {
+                    return result;
+                }
+            }
+            // 2. 如果右子节点不为空，则递归继续前序遍历
+            if (this.right != null) {
+                HeroNode result = this.right.postOrderSearch(id);
+                if (result != null) {
+                    return result;
+                }
+            }
+            System.out.println("  进入后序遍历");
+            // 3. 如果相等，则返回
+            if (this.id == id) {
+                return this;
+            }
+            return null;
+        }
     }
 
     // 编写二叉树对象
@@ -113,9 +194,42 @@ public class BinaryTreeTest {
             }
             root.postOrder();
         }
+
+        /**
+         * 前序查找
+         */
+        public HeroNode preOrderSearch(int id) {
+            if (root == null) {
+                System.out.println("二叉树为空");
+                return null;
+            }
+            return root.preOrderSearch(id);
+        }
+
+        /**
+         * 中序查找
+         */
+        public HeroNode infixOrderSearch(int id) {
+            if (root == null) {
+                System.out.println("二叉树为空");
+                return null;
+            }
+            return root.infixOrderSearch(id);
+        }
+
+        /**
+         * 后序查找
+         */
+        public HeroNode postOrderSearch(int id) {
+            if (root == null) {
+                System.out.println("二叉树为空");
+                return null;
+            }
+            return root.postOrderSearch(id);
+        }
     }
 
-    // 编写二叉树对象
+    // 二叉树测试
 
     /**
      * 前、中、后 遍历测试
@@ -165,5 +279,42 @@ public class BinaryTreeTest {
         binaryTree.infixOrder();
         System.out.println("\n 后序遍历：");
         binaryTree.postOrder();
+    }
+
+    /**
+     * 查找 id=5 的前、中、后序测试
+     */
+    @Test
+    public void fun3() {
+        // 创建节点与构建二叉树
+        HeroNode n1 = new HeroNode(1, "宋江");
+        HeroNode n2 = new HeroNode(2, "无用");
+        HeroNode n3 = new HeroNode(3, "卢俊");
+        HeroNode n4 = new HeroNode(4, "林冲");
+        HeroNode n5 = new HeroNode(5, "关胜");
+        n1.left = n2;
+        n1.right = n3;
+        n3.right = n4;
+        n3.left = n5;
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = n1;
+
+        System.out.println("找到测试：");
+        int id = 5;
+        System.out.println("\n前序遍历查找 id=" + id);
+        System.out.println(binaryTree.preOrderSearch(id));
+        System.out.println("\n中序遍历查找 id=" + id);
+        System.out.println(binaryTree.infixOrderSearch(id));
+        System.out.println("\n后序遍历查找 id=" + id);
+        System.out.println(binaryTree.postOrderSearch(id));
+
+        System.out.println("找不到测试：");
+        id = 15;
+        System.out.println("\n前序遍历查找 id=" + id);
+        System.out.println(binaryTree.preOrderSearch(id));
+        System.out.println("\n中序遍历查找 id=" + id);
+        System.out.println(binaryTree.infixOrderSearch(id));
+        System.out.println("\n后序遍历查找 id=" + id);
+        System.out.println(binaryTree.postOrderSearch(id));
     }
 }
